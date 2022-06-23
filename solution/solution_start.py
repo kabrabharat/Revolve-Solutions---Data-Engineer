6761db85419071c83273a55e0479271613aec39e
bharatkabra <bharat.kabra33@gmail.com>
2022-06-23 22:02:38 +0530

    updated solution_start.py file and added logging and exception handling

--- solution/solution_start.py
+++ solution/solution_start.py
@@ -1,11 +1,14 @@
+#Importing required libraries
 import argparse
 from typing import final
+from numpy import ndarray
 import pandas as pd
 import os
 import json
 from datetime import datetime
 import re
-
+import logging
+from typing import Callable
 
 def get_params() -> dict:
     parser = argparse.ArgumentParser(description='DataTest')
@@ -16,30 +19,61 @@ def get_params() -> dict:
     return vars(parser.parse_args())
 
 
-def read_csv_files() -> list:
+
+def configure_logging():
+    '''
+    Configuring log inside logs folder.
+
+    '''
+
+    # creating logs folder
+    if os.path.exists('./logs') == False:
+        try:
+            os.mkdir('./logs')
+        except OSError as error:
+            logging.error(error)
+    
+    # opening file in write mode to clear content already available
+    with open('./logs/solution_start.log', 'w'):
+        pass
+
+    #config
+    logging.basicConfig(filename='./logs/solution_start.log', level=logging.INFO, format='%(asctime)s : %(levelname)s : %(message)s')
+    logging.info('logging configured - Start')
+    
+
+def read_csv_files(params : dict) -> list:
     '''
     Read the customers and products csv files in dataframe
     and return the list of dataframes.
 
             Parameters:
-                    None
+                    params (dict): dictionary containing the input and output parameters
 
             Returns:
                     list of dataframes (list): list of customer and product dataframe
     '''
     
-    params = get_params()
+    logging.info('Inside read_csv_files() function')
 
     # dataframe for customer csv file
-    customer_df = pd.read_csv(str(params["customers_location"]))
-    
+    logging.info('Reading the customers.csv file into pandas->dataframe')
+    try:
+        customer_df : pd.DataFrame = pd.read_csv(str(params["customers_location"]))
+    except IOError as error:
+        logging.error(error)
+
     # dataframe for product csv file
-    product_df = pd.read_csv(str(params["products_location"]))
-    
+    logging.info('Reading the products.csv file into pandas->dataframe')
+    try:
+        product_df : pd.DataFrame = pd.read_csv(str(params["products_location"]))
+    except IOError as error:
+        logging.error(error)
+
     return [customer_df, product_df]
 
 
-def separate_column_elements(transaction_raw_df) -> pd.DataFrame:
+def separate_column_elements(transaction_raw_df : pd.DataFrame) -> pd.DataFrame:
     '''
     Separate the list or dictionary present/kept in 'basket' column
     and return whole dataframe
@@ -51,61 +85,71 @@ def separate_column_elements(transaction_raw_df) -> pd.DataFrame:
                     transaction_df (Pandas: DataFrame): Preprocessed transactions dataframe
     '''
 
+    logging.info('Inside separate_column_elements() function for basket column values division and separation')
+
     # exploding basket column's data into individual row as it contain list of dictionary
-    intermediate_df = transaction_raw_df.explode('basket').reset_index(drop=True)
+    intermediate_df : pd.DataFrame = transaction_raw_df.explode('basket').reset_index(drop=True)
     
     # applying pd.Series to each row of "basket" column as it contains dictionary, to achieve further atomicity
-    temp_df = intermediate_df['basket'].apply(pd.Series)
+    temp_df : pd.DataFrame = intermediate_df['basket'].apply(pd.Series)
 
     # now adding separate columns for each element of temp_df to intermediate_df(main df)
     intermediate_df[["product_id", "price"]] = temp_df
 
     # dropping the useless basket column (as we now having it's data separately)
-    transaction_df = intermediate_df.drop(['basket'], axis=1)
+    transaction_df : pd.DataFrame = intermediate_df.drop(['basket'], axis=1)
+
+    logging.info('created the separate columns for values inside basket column, and deleted basket column')
 
     return transaction_df
 
-def read_transaction_json_files(transaction_dirs) -> pd.DataFrame:
+def read_transaction_json_files(transaction_dirs : list, params : dict) -> pd.DataFrame:
     '''
     Read the transactions json files in dataframe
     and return the dataframes.
 
             Parameters:
                     transaction_dirs (list): list of directories containing transactions.json file
+                    params (dict): dictionary containing the input and output parameters
 
             Returns:
                     transaction_df (Pandas: DataFrame): transactions dataframe
     '''
 
-    # list to store all the JSON lines
-    transaction_list = []
+    logging.info('Inside read_transaction_json_files() function')
 
+    # list to store all the JSON lines
+    transaction_list : list = []
+
+    logging.info('Iterating over the list of transaction directories')
     # iterating over folders for transactions.json file
     for transaction in transaction_dirs:
 
+        logging.info('Reading transactions.json file inside '+ str(transaction) + ' folder')
         # opening json file
-        with open("./input_data/starter/transactions/"+ str(transaction) +"/transactions.json") as f:
+        with open(str(params['transactions_location'])+ str(transaction) +"/transactions.json") as f:
             # spliting and storing each line of file into list
-            lines = f.read().splitlines()
+            lines : list = f.read().splitlines()
 
             # storing for each files data into common list
             transaction_list.extend(lines)
 
+    logging.info('Converting Lists of JSON files into Pandas->DataFrame')
     # converting list into DataFrame
-    intermediate_df = pd.DataFrame(transaction_list)
+    intermediate_df : pd.DataFrame = pd.DataFrame(transaction_list)
 
     # giving temperory column name as json_element
     intermediate_df.columns = ['json_element']
 
     # applying json.loads function on each row, as each row is a json file and normalizing it to dataframe
-    transaction_raw_df = pd.json_normalize(intermediate_df['json_element'].apply(json.loads))
+    transaction_raw_df : pd.DataFrame = pd.json_normalize(intermediate_df['json_element'].apply(json.loads))
 
     # exploding and separating elements in "basket" column to achieve atomicity
-    transaction_df = separate_column_elements(transaction_raw_df)
+    transaction_df : pd.DataFrame = separate_column_elements(transaction_raw_df)
 
     return transaction_df
 
-def sorted_alphanumeric_Ids(customers) -> list:
+def sorted_alphanumeric_Ids(customers : list) -> list:
     '''
     Sort the AlphaNumeric Customer Ids(String)
 
@@ -116,31 +160,34 @@ def sorted_alphanumeric_Ids(customers) -> list:
                     sorted customers (list): list of sorted customer_ids
     '''
     # anonymous function to convert numbers(string) to int after split
-    convert = lambda text: int(text) if text.isdigit() else text
+    convert : Callable[[str], object] = lambda text: int(text) if text.isdigit() else text
     # anonymous function to split numbers from each id
     alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
     return sorted(customers, key = alphanum_key)
 
 
-def generate_json(df, name):
+def generate_json(df : pd.DataFrame, name : str, params : dict):
 
     '''
     Create JSON files using weekly data
 
             Parameters:
                     df (Pandas: DataFrame): final weekly dataframe
+                    params (dict): dictionary containing the input and output parameters
 
             Returns:
                     Nothing
     '''
 
+    logging.info('Inside generate_json() Function')
+
     # list of unique customers
-    customers = list(df['customer_id'].unique())
+    customers : list = list(df['customer_id'].unique())
 
     #sort customers
     customers = sorted_alphanumeric_Ids(customers)
     
-    output_dict = {}
+    output_dict : dict = {}
 
     for customer in customers:
         output_dict[customer] = {}
@@ -163,33 +210,43 @@ def generate_json(df, name):
             else:
                 output_dict[customer]['product_category'] = []
     
-    # to get output location parameter
-    params = get_params()
 
     # checking if the given directory already present in the system (if not creating it)
     if os.path.exists(params['output_location']) == False:
-        os.makedirs(params['output_location'], exist_ok=True)
-        
-    # dumping dictionary to JSON file with naming convension --> Week_<<sunday_date>>.json
-    with open('./output_data/outputs/Week_'+str(name).split(' ')[0]+'.json', 'w', encoding='utf-8') as f:
-        json.dump(output_dict, f, ensure_ascii=False, indent=4)
+        try:
+            os.makedirs(params['output_location'], exist_ok=True)
+        except OSError as error:
+            logging.error(error)
+
+    logging.info('Storing Week_' +str(name).split(' ')[0]+'.json file')    
+
+    try:
+        # dumping dictionary to JSON file with naming convension --> Week_<<sunday_date>>.json
+        with open(str(params['output_location']) + 'Week_'+str(name).split(' ')[0]+'.json', 'w', encoding='utf-8') as f:
+            json.dump(output_dict, f, ensure_ascii=False, indent=4)
+    except IOError as error:
+        logging.error(error)
     
 
-def segregate_weekly(final_df):
+def segregate_weekly(final_df : pd.DataFrame, params : dict):
 
     '''
     Segregate the DataFrame Week-wise.
 
             Parameters:
                     final_df (Pandas: DataFrame): Final DataFrame with whole data
+                    params (dict): dictionary containing the input and output parameters
 
             Returns:
                     Nothing
     '''
 
+    logging.info('Inside segregate_weekly() function')
+
     # converting the dataframe object to DateTime type Format
     final_df['date_of_purchase'] = [datetime.strptime(dates.split(" ")[0], '%Y-%m-%d') for dates in final_df['date_of_purchase']]
 
+    logging.info('Sorting final dataframe date-wise')
     # sorting the dataframe date-wise
     final_df = final_df.sort_values(by='date_of_purchase')
 
@@ -198,12 +255,12 @@ def segregate_weekly(final_df):
 
     # initializing start date if it's not sunday
     if final_df['date_of_purchase'][0].isoweekday() != 7:
-        weeks = [final_df['date_of_purchase'][0]]
+        weeks : list = [final_df['date_of_purchase'][0]]
     else:
         weeks = []
     
     # variable to remove duplicate dates (by storing previous date and check with it)
-    prev_date = str(final_df['date_of_purchase'][0])
+    prev_date : str = str(final_df['date_of_purchase'][0])
     
     # checking for sundays
     for element in final_df['date_of_purchase']:
@@ -215,17 +272,18 @@ def segregate_weekly(final_df):
     if final_df['date_of_purchase'][len(final_df)-1].isoweekday() != 7:
         weeks.append(final_df['date_of_purchase'][len(final_df)-1])
 
+    logging.info('Iteration for Weekly Separation of DataFrame')
     # iterating over the weeks variable for weekly separation and passing it to generate_json() function 
     for date_index in range(len(weeks)-1):
         
         if date_index == 0:
-            generate_json(final_df[(final_df['date_of_purchase'] >= weeks[date_index]) & (final_df['date_of_purchase'] <= weeks[date_index + 1])], str(weeks[date_index + 1])) 
+            generate_json(final_df[(final_df['date_of_purchase'] >= weeks[date_index]) & (final_df['date_of_purchase'] <= weeks[date_index + 1])], str(weeks[date_index + 1]), params) 
         else:
-            generate_json(final_df[(final_df['date_of_purchase'] > weeks[date_index]) & (final_df['date_of_purchase'] <= weeks[date_index + 1])], str(weeks[date_index + 1]))
+            generate_json(final_df[(final_df['date_of_purchase'] > weeks[date_index]) & (final_df['date_of_purchase'] <= weeks[date_index + 1])], str(weeks[date_index + 1]), params)
 
 
 
-def merge_dataframes(product_df, customer_df, transaction_df) -> pd.DataFrame:
+def merge_dataframes(product_df : pd.DataFrame, customer_df : pd.DataFrame, transaction_df : pd.DataFrame) -> pd.DataFrame:
     
     '''
     Merging relevant columns of product and customer dataframe with transaction dataframe.
@@ -239,34 +297,45 @@ def merge_dataframes(product_df, customer_df, transaction_df) -> pd.DataFrame:
                     final_df (Pandas: DataFrame): final merged dataframe
     '''
 
-    # merging transactions dataframe with "product_category" column of product dataframe on same product_id
-    intermediate_df = transaction_df.merge(product_df[["product_id", "product_category"]], on="product_id", how="left")
+    logging.info('Inside merge_dataframes() function')
 
+    logging.info('Merging relevant Product Columns to transactions dataframe')
+    # merging transactions dataframe with "product_category" column of product dataframe on same product_id
+    intermediate_df : pd.DataFrame = transaction_df.merge(product_df[["product_id", "product_category"]], on="product_id", how="left")
+
+    logging.info('Merging relevant Customer Columns to transactions dataframe')
     # merging resultant dataframe with "loyalty_score" column of customer dataframe on same customer_id
-    final_df = intermediate_df.merge(customer_df[["customer_id", "loyalty_score"]], on="customer_id", how="left")
+    final_df : pd.DataFrame = intermediate_df.merge(customer_df[["customer_id", "loyalty_score"]], on="customer_id", how="left")
     
     return final_df
 
 
 def main():
     
-    # reading the customer and product csv files in dataframe
-    customer_df, product_df = read_csv_files()
+    # logging configuration
+    configure_logging()
 
+    logging.info('Getting default or input arguments')
     # get parameters
-    params = get_params()
+    params : dict = get_params()
+
+    # reading the customer and product csv files in dataframe
+    customer_df, product_df = read_csv_files(params)
 
     # reading transaction directories in a list
-    transaction_dirs = os.listdir(str(params["transactions_location"]))
+    logging.info('Listing all the directories inside '+ str(params["transactions_location"]))
+    transaction_dirs : list = os.listdir(str(params["transactions_location"]))
 
     # reading transactions json file in dataframe
-    transaction_df = read_transaction_json_files(transaction_dirs)
+    transaction_df : pd.DataFrame = read_transaction_json_files(transaction_dirs, params)
 
     # merge product and customer dataframes to transaction dataframe for product_category and loyalty_score
-    final_df = merge_dataframes(product_df, customer_df, transaction_df)
+    final_df : pd.DataFrame = merge_dataframes(product_df, customer_df, transaction_df)
 
     # week-wise segregation
-    segregate_weekly(final_df)
+    segregate_weekly(final_df, params)
+
+    logging.info('Done!')
 
 
 if __name__ == "__main__":
---
+++ test.py
@@ -0,0 +1,3 @@
+import pytest
+from solution import solution_start
+    
